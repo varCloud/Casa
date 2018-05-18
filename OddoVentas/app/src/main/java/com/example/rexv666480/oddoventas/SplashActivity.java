efe8d1d6169880f77cf127a827b534555fc9de48
victor <Abcde1>
2018-05-18 16:40:13 -0500

    ife brisa

--- OddoVentas/app/src/main/java/com/example/rexv666480/oddoventas/SplashActivity.java
+++ OddoVentas/app/src/main/java/com/example/rexv666480/oddoventas/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.example.rexv666480.oddoventas;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -13,7 +16,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private final int SPLASH_DISPLAY_LENGTH = 18000;
+    private View mCardFrontLayout;
+    private View mCardBackLayout;
+    private AnimatorSet mSetRightOut;
+    private AnimatorSet mSetLeftIn;
+    private boolean mIsBackVisible = false;
 
     @BindView(R.id.imageO1)
     ImageView imageO1;
@@ -34,8 +42,55 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, SPLASH_DISPLAY_LENGTH);
 
-        Animation startRotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.android_rotate_animation);
+       /* Animation startRotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.android_rotate_animation);
         imageO1.startAnimation(startRotateAnimation);
+        */
+        findViews();
+        loadAnimations();
+        changeCameraDistance();
+        flipCard();
+        try {
+            Thread.sleep(5000);
+            flipCard();
+            Thread.sleep(5000);
+            flipCard();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+
+
+    }
+    private void findViews() {
+        mCardBackLayout = findViewById(R.id.card_back);
+        mCardFrontLayout = findViewById(R.id.card_front);
+    }
+
+    private void changeCameraDistance() {
+        int distance = 8000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mCardFrontLayout.setCameraDistance(scale);
+        mCardBackLayout.setCameraDistance(scale);
+    }
+
+    private void loadAnimations() {
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.out_animation);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.in_animation);
+    }
+
+    public void flipCard() {
+        if (!mIsBackVisible) {
+            mSetRightOut.setTarget(mCardFrontLayout);
+            mSetLeftIn.setTarget(mCardBackLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = true;
+        } else {
+            mSetRightOut.setTarget(mCardBackLayout);
+            mSetLeftIn.setTarget(mCardFrontLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = false;
+        }
     }
 }
